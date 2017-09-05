2ab79c5216b6a6f56246ba71d58c1494c366e091
Ryan Hubbard <ryandhubbard@gmail.com>
2017-09-05 12:08:30 -0700

    Created index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: WP-Media-CDN
+ * Version: 1.0
+ * Plugin URI:
+ * Description: WP-Media-CDN
+ * Author URI:
+ * Requires at least:
+ * Tested up to:
+ *
+ * Text Domain:
+ * Domain Path:
+ *
+ * @package WordPress
+ * @author imForza
+ * @since 1.0.0
+ */
+
+?>
