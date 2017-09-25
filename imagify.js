46e6b14983526e45009bd5d4c654a6ad422cc3ae
Ryan Hubbard <ryandhubbard@gmail.com>
2017-09-25 10:52:03 -0700

    Started created send_to_imagify function

--- imagify.js
+++ imagify.js
@@ -1,12 +1,22 @@
 var token ="d0e020dda55a01c8e753214e3b298376e875f9cf";
 
-var nodeImagifyAPI = require("node-imagify-api");
+send_to_imagify("test","test","test",token);
 
-var options = {'ultra':true
-                , 'resize': {'width':150}};
 
-var apiCallback = function (result) {
-    console.log("result :"+JSON.stringify(result));
+
+
+
+function send_to_imagify(title, image, sizes, token) {
+
+  var nodeImagifyAPI = require("node-imagify-api");
+
+  var options = {'ultra':true
+                  , 'resize': {'width':150, 'height':10000}};
+
+  var apiCallback = function (result) {
+      console.log("result :"+JSON.stringify(result));
+  }
+
+  nodeImagifyAPI.uploadImage(token,options,"test_image.jpg",apiCallback);
+
 }
-
-nodeImagifyAPI.uploadImage(token,options,"test_image.jpg",apiCallback);
