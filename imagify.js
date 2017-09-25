0c85e173a7e16459aa1e3505e882ccefe1517eb6
Ryan Hubbard <ryandhubbard@gmail.com>
2017-09-25 14:24:27 -0700

    Created test json array

--- imagify.js
+++ imagify.js
@@ -1,17 +1,17 @@
 var token ="d0e020dda55a01c8e753214e3b298376e875f9cf";
 
-send_to_imagify("test","test","test",token);
+var sizes = '{"thumbnail": { width": 150, "height":150,"crop":true}, "medium": { width": 150, "height":150,"crop":true}';
 
 
-
-
+send_to_imagify("test","test",sizes,token);
 
 function send_to_imagify(title, image, sizes, token) {
 
   var nodeImagifyAPI = require("node-imagify-api");
 
-  var options = {'ultra':true
-                  , 'resize': {'width':150, 'height':10000}};
+  var options = {'ultra':true,
+                   'resize': {'width':sizes[0].width, 'height':sizes[0].height}
+                };
 
   var apiCallback = function (result) {
       console.log("result :"+JSON.stringify(result));
