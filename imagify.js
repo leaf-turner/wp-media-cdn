313a4dbd3cd8d9034516cf56ce67ad8acec0984c
Ryan Hubbard <ryandhubbard@gmail.com>
2017-09-27 13:18:15 -0700

    #5

--- imagify.js
+++ imagify.js
@@ -1,22 +1,26 @@
-var token ="d0e020dda55a01c8e753214e3b298376e875f9cf";
+var token = "d0e020dda55a01c8e753214e3b298376e875f9cf";
 
-var sizes = '{"thumbnail": { width": 150, "height":150,"crop":true}, "medium": { width": 150, "height":150,"crop":true}';
+var sizes = JSON.parse('{"thumbnail": {"width": "500", "height":"500","crop":"true"}, "medium": {"width": "500", "height":"500","crop":"true"}}');
 
-
-send_to_imagify("test","test",sizes,token);
+send_to_imagify("test", "test", sizes, token);
 
 function send_to_imagify(title, image, sizes, token) {
 
   var nodeImagifyAPI = require("node-imagify-api");
 
-  var options = {'ultra':true,
-                   'resize': {'width':sizes[0].width, 'height':sizes[0].height}
-                };
+  for (var i in sizes) {
+      var options = {
+        'ultra': true,
+        'resize': {
+          'width': sizes[i].width,
+          'height': sizes[i].height
+        }
+      };
 
-  var apiCallback = function (result) {
-      console.log("result :"+JSON.stringify(result));
+      var apiCallback = function(result) {
+        console.log("result :" + JSON.stringify(result));
+      }
+
+      nodeImagifyAPI.uploadImage(token, options, "test_image.jpg", apiCallback);
   }
-
-  nodeImagifyAPI.uploadImage(token,options,"test_image.jpg",apiCallback);
-
 }
