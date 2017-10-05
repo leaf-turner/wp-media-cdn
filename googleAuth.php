bfdfc736bf48b42f5c93dc0ae38fc436b4fe9a7e
Sight29 <d.satbir@ymail.com>
2017-10-05 09:00:06 -0700

    Update googleAuth.php

--- googleAuth.php
+++ googleAuth.php
@@ -4,6 +4,6 @@
 $serverName = $_SERVER['HTTP_HOST']?$_SERVER['HTTP_HOST']: $_SERVER["SERVER_NAME"];
 $id           = str_replace('.','-',$server_name);
 $project_name = trim(substr($id, 0, 30), '-');
-$project_id   = trim(substr($id, 0, 23), '-') . "-" . rand(100000, 999999);
+$project_id   = trim(substr($id, 0, 23), '-');
 $bucket_id    = trim("stateless-" . substr($id, 0, 20), '-');
 ?>
